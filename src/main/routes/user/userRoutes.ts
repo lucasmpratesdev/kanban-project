c8a17d82a6838cba8f693979da720a740b284d2a
Lucas Prates <lucasmpratesdev@gmail.com>
2025-05-18 02:46:41 -0300

    feat: add auth in userroute

--- src/main/routes/user/userRoutes.ts
+++ src/main/routes/user/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { adaptRoute, addCrudRoutes } from '../../adapters'
 import { makeUserCrudController } from "../../factories/user/UserCrudFactory"
+import { auth } from '../../../main/middlewares/auth'
 
 export default (router: Router): void => {
     const path = '/user'
     // router.post('/login', adaptRoute(makeLoginController()))
 
-    addCrudRoutes(router, path, makeUserCrudController())
+    addCrudRoutes(router, path, makeUserCrudController(), auth)
 }
